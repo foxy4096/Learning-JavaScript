b2d3f42bb452af80754bc03c9cad4a4beaa3c328
Aditya Priyadarshi <54215788+foxy4096@users.noreply.github.com>
2021-10-17 21:49:16 +0530

    New Code

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
-console.log("Hello World");
+// console.log("Hello World");
 
-const PI = 3.14;
+// const PI = 3.14;
 
-var area, radius = 45;
+// var area, radius = 45;
 
-area = PI * radius * radius;
+// area = PI * radius * radius;
 
-console.log(area)
+// console.log(area)
+
+// var my_list = ["Aditya", "Doc", "Django"];
+// let name = "Aditya";
+// console.log(my_list);
+// console.log(typeof my_list);
+
+// console.log(console.log().toString());
+
+
+// function add(num1, num2){
+//     return num1 + num2;
+// }
+
+// console.log(add(15,65));
+
+
+// let person = {
+//     fname: "Aditya",
+//     lname: "Priyadarshi",
+//     age: 16,
+//     languages: ["Python", "Java", "JavaScript", "Hindi"],
+//     getFullName: function () {
+//         return this.fname + " " + this.lname;
+//     }
+// }
+
+// console.log(person.getFullName())
